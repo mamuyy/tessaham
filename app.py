bc00cd49409e74a32ae34d5b731ab783882d9cde
mamuyy <mamuyuwono3@gmail.com>
2025-04-06 20:54:43 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Tes Saham Streamlit", layout="wide")
+st.title("📊 Aplikasi Analisis Saham")
+st.subheader("Hello, ini Streamlit app dari repo tessaham 🚀")
+
+st.write("""
+Ini adalah aplikasi percobaan untuk menampilkan hasil clone dan deploy dari GitHub ke Streamlit.
+Silakan sesuaikan nanti dengan data portofolio, chart saham, atau analisis Mean-Variance dari Colab!
+""")
